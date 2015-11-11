8bc4d8a645ae86ab0e5538e4ba4c69890b6ea579
Oscar Esteve <oscar.esteve@ext.produban.com>
2015-11-11 20:43:45 +0100

    Refactorización del repositorio

--- app/scripts/services/bands.js
+++ app/scripts/services/bands.js
@@ -8,35 +8,36 @@
  * Factory in the angularRocksApp.
  */
 angular.module('angularRocksApp')
-  .factory('bands', function () {
-    // Service logic
-    // ...
+        .factory('bands', function () {
 
-    var meaningOfLife = 42;
-    
-    var bands = [];
-    
-    bands.push({
-        name: 'The Rolling Stones',
-        slug: 'the-rolling-stones',
-        history: 'La historia the-rolling-stones'
-    });
+            var bands = [{
+                    name: 'The Rolling Stones',
+                    slug: 'the-rolling-stones',
+                    history: 'La historia the-rolling-stones'
+                }, {
+                    name: 'The Beatles',
+                    slug: 'the-beatles',
+                    history: 'La historia de The Beatles'
+                }, {
+                    name: 'Queen',
+                    slug: 'queen',
+                    history: 'La historia de Queen'
+                }];
 
-    bands.push({
-        name: 'The Beatles',
-        slug: 'the-beatles',
-        history: 'La historia de The Beatles'
-    });
-    bands.push({
-        name: 'Queen',
-        slug: 'queen',
-        history: 'La historia de Queen'
-    });
 
-    // Public API here
-    return {
-      query: function () {
-        return bands;
-      }
-    };
-  });
+            return {
+                query: function () {
+                    return bands;
+                },
+                get: function (bandName) {
+                    for (var i = 0; i < bands.length; i++) {
+                        if (bands[i].slug === bandName) {
+                            return bands[i];
+                        }
+                    }
+
+                    return null;
+
+                }
+            };
+        });
