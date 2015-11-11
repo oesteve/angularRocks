35fe40d41adabcc4af9b1ff4d085f3e231290b4b
Oscar Esteve <oscar.esteve@ext.produban.com>
2015-11-11 23:11:47 +0100

    Mejorar el l&f, origen de scss http://bootswatch.com/journal/

--- app/scripts/services/bands.js
+++ app/scripts/services/bands.js
@@ -13,14 +13,23 @@ angular.module('angularRocksApp')
             var bands = [{
                     name: 'The Rolling Stones',
                     slug: 'the-rolling-stones',
+                    logo: 'images/rolling_stones_logo.jpg',
+                    imgLarge: 'http://wfiles.brothersoft.com/t/the-rolling-stones-wallpaper_154083-1920x1080.jpg',
+                    resume: 'Una de las mejores bandas de rock del sigo XX',
                     history: 'La historia the-rolling-stones'
                 }, {
                     name: 'The Beatles',
                     slug: 'the-beatles',
+                    logo: 'images/beatles_logo.gif',
+                    imgLarge: 'http://www.kissfm.es/wp-content/uploads/2012/10/beatles1.jpg',
+                    resume: 'Una de las mejores bandas de rock del sigo XX',
                     history: 'La historia de The Beatles'
                 }, {
                     name: 'Queen',
                     slug: 'queen',
+                    logo: 'images/queen_logo.png',
+                    imgLarge: 'http://orig06.deviantart.net/61df/f/2008/238/0/e/queen_the_band_by_fallenzeraphine.jpg',
+                    resume: 'Una de las mejores bandas de rock del sigo XX',
                     history: 'La historia de Queen'
                 }];
 
