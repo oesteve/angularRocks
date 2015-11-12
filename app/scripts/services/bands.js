6c5a5c2aa2c5176587a6e1b7bc7107d9211d104e
Oscar Esteve <oscar.esteve@ext.produban.com>
2015-11-13 00:39:13 +0100

    Mejorando el contenido para dispositivos móviles

--- app/scripts/services/bands.js
+++ app/scripts/services/bands.js
@@ -16,7 +16,7 @@ angular.module('angularRocksApp')
                     youtube: 'Nqp2stVMVjM',
                     logo: 'images/rolling_stones_logo.jpg',
                     imgLarge: 'images/rolling_stones_large.jpg',
-                    summary: 'Banda británica de rock originaria de Londres. La banda se fundó en abril de 1962 por Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Ian Stewart y Charlie Watts',
+                    summary: 'Banda británica de rock originaria de Londres fundada en abril de 1962',
                     history: 'The Rolling Stones es una banda británica de rock originaria de Londres. La banda se fundó en abril de 1962 por Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Ian Stewart y Charlie Watts.2 Brian Jones fue despedido en 1969, falleciendo al poco tiempo, siendo reemplazado por el guitarrista Mick Taylor, que dejaría el grupo en 1975 y sería a su vez reemplazado por Ron Wood. Con el retiro de Bill Wyman en 1993 se incluyó al bajista Darryl Jones que, aunque toca con la banda desde la grabación del álbum Voodoo Lounge en 1994, no es un miembro oficial.',
                     wikipedia: 'https://es.wikipedia.org/wiki/The_Rolling_Stones',
                     keywords: ['reino unido', 'rock', 'banda',  'Brian Jones', 'Mick Jagger', 'Keith Richards', 'Bill Wyman', 'Ian Stewart', 'Charlie Watts']
@@ -26,7 +26,7 @@ angular.module('angularRocksApp')
                     youtube: 'z9ypq6_5bsg',
                     logo: 'images/beatles_logo.gif',
                     imgLarge: 'images/beatles_large.jpg',
-                    summary: 'Banda de rock inglesa activa durante la década de 1960, y reconocida como la más exitosa comercialmente y críticamente aclamada en la historia de la música popular.',
+                    summary: 'Banda de rock inglesa activa durante la década de 1960',
                     history: 'The Beatles fue una banda de rock inglesa activa durante la década de 1960, y reconocida como la más exitosa comercialmente y críticamente aclamada en la historia de la música popular Formada en Liverpool, estuvo constituida desde 1962 por John Lennon (guitarra rítmica, vocalista), Paul McCartney (bajo, vocalista), George Harrison (guitarra solista, vocalista) y Ringo Starr (batería, vocalista). Enraizada en el skiffle y el rock and roll de los años cincuenta, la banda trabajó más tarde con distintos géneros musicales, que iban desde las baladas pop hasta el rock psicodélico, incorporando a menudo elementos clásicos, entre otros, de forma innovadora en sus canciones. La naturaleza de su enorme popularidad, que había emergido primeramente con la moda de la «Beatlemanía», se transformó al tiempo que sus composiciones se volvieron más sofisticadas. Llegaron a ser percibidos como la encarnación de los ideales progresistas, extendiendo su influencia en las revoluciones sociales y culturales de la década de 1960.',
                     wikipedia: 'https://es.wikipedia.org/wiki/The_Beatles',
                     keywords: ['Beatles', 'rock', 'ingles', '60s', 'Liverpool']
@@ -36,7 +36,7 @@ angular.module('angularRocksApp')
                     youtube: 'f4Mc-NYPHaQ',
                     logo: 'images/queen_logo.jpg',
                     imgLarge: 'images/queen_large.jpg',
-                    summary: 'Una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon',
+                    summary: 'Una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury',
                     history: 'Queen es una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Freddie Mercury, que murió en 1991 y John Deacon, que se retiró en 1997), los integrantes restantes, Brian May y Roger Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún es considerada activa.',
                     wikipedia: 'https://es.wikipedia.org/wiki/Queen',
                     keywords: ['Queen', 'británica', 'banda', 'Freddie Mercury' ]
